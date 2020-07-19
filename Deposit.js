918934ff9e8014f98f4d1bb463ccff4480c0c225
Roman <romasherbina@gmail.com>
2020-07-19 21:41:36 +0300

    JS Task1,2,3

---
+++ Deposit.js
@@ -0,0 +1,41 @@
+let name= prompt("Введіть ваше ім'я:");
+let sum= Number(prompt("Введіть сумму для внессеняя на депозит:"));
+let numbeMonths = Number(prompt("Введіть кількісь місяців:"));
+let stavka;
+let monthlyPercentage;
+let income;
+
+if ( numbeMonths<6)
+{
+    stavka=(sum*15)/100;
+    monthlyPercentage=stavka/12;
+    income=monthlyPercentage*numbeMonths;
+
+    alert(`Шановний ${name} Ви внесли:${sum} грн.  на ${numbeMonths} місяців під 15% річних, Ваш дохід буде ${income} грн.`);
+    console.log(`Шановний ${name} Ви внесли:${sum} грн. на ${numbeMonths} місяців під 15% річних, Ваш дохід буде ${income} грн.`);
+}
+
+else if(numbeMonths>=6 && numbeMonths<=9)
+{
+    stavka=(sum*16)/100;
+    monthlyPercentage=stavka/12;
+    income=monthlyPercentage*numbeMonths;
+    
+    alert(`Шановний ${name} Ви внесли:${sum} грн.  на ${numbeMonths} місяців під 16% річних, Ваш дохід буде ${income} грн.`);
+    console.log(`Шановний ${name} Ви внесли:${sum} грн.  на ${numbeMonths} місяців під 16% річних, Ваш дохід буде ${income} грн.`);
+}
+
+
+
+else if(numbeMonths>9)
+{
+    stavka=(sum*17)/100;
+    monthlyPercentage=stavka/12;
+    income=monthlyPercentage*numbeMonths;
+
+    alert(`Шановний ${name} Ви внесли:${sum} грн.  на ${numbeMonths} місяців під 17% річних, Ваш дохід буде ${income} грн.`);
+    console.log(`Шановний ${name} Ви внесли:${sum} грн.   на ${numbeMonths} місяців під 17% річних, Ваш дохід буде ${income} грн.`);
+
+}
+
+
---
+++ Variables.js
@@ -0,0 +1,23 @@
+let a = Number( prompt("Ведіть значення змінної a:"));
+let b = Number( prompt("Ведіть значення змінної b:"));
+
+
+if(a>3 && a<12)
+{
+   a=true;
+
+}
+
+
+if (b>=5 && b<13)
+{
+    b=true;
+}
+
+if(a==b)
+{
+    alert("Вірно");
+}
+else{
+    alert("Ніверно");
+}
